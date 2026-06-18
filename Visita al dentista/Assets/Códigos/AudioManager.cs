eb985c680169e0501d7109cb86de954fb45af19a
DiGesu <joaquinhermosilla23@gmail.com>
2026-06-18 02:52:39 -0400

    Implementaciones finales
    Audiomanager, fondos win/lose y orden de jerarquia

--- Visita al dentista/Assets/Códigos/AudioManager.cs
+++ Visita al dentista/Assets/Códigos/AudioManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI; // ¡IMPORTANTE! Necesario para usar componentes Slider
 
 public class AudioManager : MonoBehaviour
 {
-    // PROPIEDAD GLOBAL AUTOMÁTICA: Con "I" mayúscula y protegida para todo el proyecto
     public static AudioManager Instancia { get; private set; }
 
     [Header("Mixer Principal")]
@@ -13,9 +13,13 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource fuenteMusica;
     [SerializeField] private AudioSource fuenteSFX;
 
+    // --- NUEVO: Referencias a los sliders del menú ---
+    [Header("UI Sliders")]
+    [SerializeField] private Slider sliderMusica;
+    [SerializeField] private Slider sliderSFX;
+
     void Awake()
     {
-        // Usamos Instancia con mayúscula de forma coherente internamente
         if (Instancia == null)
         {
             Instancia = this;
@@ -25,6 +29,13 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    // --- NUEVO: Sincronizar volumen al iniciar ---
+    void Start()
+    {
+        if (sliderMusica != null) CambiarVolumenMusica(sliderMusica.value);
+        if (sliderSFX != null) CambiarVolumenSFX(sliderSFX.value);
     }
 
     public void CambiarVolumenMusica(float valorSlider)
